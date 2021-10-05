3dc5de8e8dd5780132d89e4441389b625f54ceaa
ejaszczuk <emj1054@gmail.com>
2021-10-05 21:16:02 +0200

    Add Codegen implementation for '+' and '.'

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,43 @@ use inkwell::execution_engine;
 use inkwell::execution_engine::{ExecutionEngine, JitFunction};
 use inkwell::module::{Linkage, Module};
 use inkwell::targets::{InitializationConfig, Target};
+use inkwell::types::{FunctionType, IntType, PointerType};
+use inkwell::values::AnyValue;
 use inkwell::values::{BasicValueEnum, IntValue};
 use inkwell::AddressSpace;
 use inkwell::OptimizationLevel;
 
-// moze struct typu Types gdzie bede mial wszystkie
-// array types itp itd a potem to wjebac jako obiekt do Codegen
+struct Types<'ctx> {
+    i32_type: IntType<'ctx>,
+    i64_type: IntType<'ctx>,
+    i8_type: IntType<'ctx>,
+    i8_ptr_type: PointerType<'ctx>,
+    main_fn_type: FunctionType<'ctx>,
+    calloc_fn_type: FunctionType<'ctx>,
+    putchar_fn_type: FunctionType<'ctx>,
+}
+
+impl<'ctx> Types<'ctx> {
+    fn new(context: &'ctx Context) -> Self {
+        let i32_type = context.i32_type();
+        let i64_type = context.i64_type();
+        let i8_type = context.i8_type();
+        let i8_ptr_type = i8_type.ptr_type(AddressSpace::Generic);
+        let main_fn_type = i32_type.fn_type(&[], false);
+        let calloc_fn_type = i8_ptr_type.fn_type(&[i64_type.into(), i64_type.into()], false);
+        let putchar_fn_type = i32_type.fn_type(&[i32_type.into()], false);
+
+        Types {
+            i32_type,
+            i64_type,
+            i8_type,
+            i8_ptr_type,
+            main_fn_type,
+            calloc_fn_type,
+            putchar_fn_type,
+        }
+    }
+}
 
 struct Codegen<'ctx> {
     input: Node,
@@ -26,45 +57,90 @@ struct Codegen<'ctx> {
     module: Module<'ctx>,
     builder: Builder<'ctx>,
     execution_engine: ExecutionEngine<'ctx>,
+    types: Types<'ctx>,
 }
 
 impl<'ctx> Codegen<'ctx> {
     fn generate_llvm(&self) {
-        let i32_type = self.context.i32_type();
-        let fn_type = i32_type.fn_type(&[], false);
-        let array_type = i32_type.array_type(3);
+        // Values
+        let main_fn_value = self
+            .module
+            .add_function("main", self.types.main_fn_type, None);
 
-        let fn_value = self.module.add_function("main", fn_type, None);
-        let entry = self.context.append_basic_block(fn_value, "entry");
+        let calloc_fn_value =
+            self.module
+                .add_function("calloc", self.types.calloc_fn_type, Some(Linkage::External));
 
-        self.builder.position_at_end(entry);
+        let putchar_fn_value = self.module.add_function(
+            "putchar",
+            self.types.putchar_fn_type,
+            Some(Linkage::External),
+        );
 
-        let array_alloca = self.builder.build_alloca(array_type, "arr");
-        let array = self
+        let entry_block = self.context.append_basic_block(main_fn_value, "entry");
+        self.builder.position_at_end(entry_block);
+
+        // Allocate data* and ptr*
+        let data_alloca = self
             .builder
-            .build_load(array_alloca, "arr_l")
-            .into_array_value();
+            .build_alloca(self.types.i8_ptr_type, "data_alloca");
+        let ptr_alloca = self
+            .builder
+            .build_alloca(self.types.i8_ptr_type, "ptr_alloca");
 
-        let const_int1 = i32_type.const_int(2, false);
-        let const_int2 = i32_type.const_int(3, false);
-        let const_int3 = i32_type.const_int(4, false);
-        self.builder
-            .build_insert_value(array, const_int1, 0, "insert");
-        self.builder
-            .build_insert_value(array, const_int2, 1, "insert");
-        self.builder
-            .build_insert_value(array, const_int3, 2, "insert");
+        let mem_size_const = self.types.i64_type.const_int(1024, false);
+        let element_size_const = self.types.i64_type.const_int(1, false);
+
+        let calloc_data = self.builder.build_call(
+            calloc_fn_value,
+            &[mem_size_const.into(), element_size_const.into()],
+            "calloc_call",
+        );
+
+        let calloc_data_result: Result<_, _> = calloc_data.try_as_basic_value().flip().into();
+        let calloc_data_basic_val = calloc_data_result
+            .map_err(|_| "calloc returned void")
+            .unwrap();
+
+        self.builder.build_store(data_alloca, calloc_data_basic_val);
+        self.builder.build_store(ptr_alloca, calloc_data_basic_val);
 
         match &self.input {
             Node::Expr(expr_val) => {
                 for x in expr_val.iter() {
-                    // println!("{:?}", x);
                     match x {
                         Node::PlusNode => {
-                            // tutaj generujemy
+                            let amount_const = self.types.i8_type.const_int(1 as u64, false);
+                            let ptr_load = self
+                                .builder
+                                .build_load(data_alloca, "load_ptr")
+                                .into_pointer_value();
+                            let ptr_val = self.builder.build_load(ptr_load, "ptr_val");
+                            let result = self.builder.build_int_add(
+                                ptr_val.into_int_value(),
+                                amount_const,
+                                "add_data_ptr",
+                            );
+                            self.builder.build_store(ptr_load, result);
                         }
                         Node::PrintCurrPosNode => {
-                            // bierzemy current value pointer
+                            let pc_char = self.builder.build_load(
+                                self.builder
+                                    .build_load(data_alloca, "ptr_val")
+                                    .into_pointer_value(),
+                                "load_ptr_val",
+                            );
+
+                            let sext = self.builder.build_int_s_extend(
+                                pc_char.into_int_value(),
+                                self.context.i32_type(),
+                                "putchar_sign",
+                            );
+                            self.builder.build_call(
+                                putchar_fn_value,
+                                &[sext.into()],
+                                "putchar_call",
+                            );
                         }
                         _ => {}
                     }
@@ -75,8 +151,15 @@ impl<'ctx> Codegen<'ctx> {
             }
         }
 
-        let pointee = i32_type.const_int(0, false);
-        self.builder.build_return(Some(&pointee));
+        // Free the pointer
+        self.builder.build_free(
+            self.builder
+                .build_load(data_alloca, "load")
+                .into_pointer_value(),
+        );
+
+        let return_zero_const = self.types.i32_type.const_int(0, false);
+        self.builder.build_return(Some(&return_zero_const));
 
         self.module.print_to_stderr();
     }
@@ -96,12 +179,15 @@ fn main() {
     let builder = context.create_builder();
     let execution_engine = module.create_execution_engine().unwrap();
 
+    let types = Types::new(&context);
+
     let cdg = Codegen {
         input: nodes,
         context: &context,
         module,
         builder,
         execution_engine,
+        types,
     };
 
     cdg.generate_llvm();
