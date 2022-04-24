dddb9a3f66d216441b194bf1b67f48990c5055e4
remique <emj1054@gmail.com>
2022-04-24 22:17:36 +0200

    Add basic Parser tests

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,8 @@
 use crate::lexer::*;
 
-#[derive(Debug, Clone)]
+use std::{fmt, fs, fs::File, io::Write, path::Path};
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum Node {
     PlusNode,
     MinusNode,
@@ -24,69 +26,110 @@ impl Parser {
     }
 
     pub fn parse_expr(&mut self, vect: &[Token], is_loop: bool) -> Node {
-        let mut x: Vec<Node> = Vec::new();
+        let mut result: Vec<Node> = Vec::new();
 
-        let mut iq = vect.iter();
-
-        while let Some(tok) = iq.next() {
+        let mut vect_iter = vect.iter();
+        while let Some(tok) = vect_iter.next() {
             match tok {
                 Token::PlusToken => {
-                    x.push(Node::PlusNode);
-                    self.position += 1;
+                    result.push(Node::PlusNode);
                 }
                 Token::MinusToken => {
-                    x.push(Node::MinusNode);
-                    self.position += 1;
+                    result.push(Node::MinusNode);
                 }
                 Token::SmallerThanToken => {
-                    x.push(Node::DecrementPtrNode);
-                    self.position += 1;
+                    result.push(Node::DecrementPtrNode);
                 }
                 Token::GreaterThanToken => {
-                    x.push(Node::IncrementPtrNode);
-                    self.position += 1;
+                    result.push(Node::IncrementPtrNode);
                 }
                 Token::DotToken => {
-                    x.push(Node::PrintCurrPosNode);
-                    self.position += 1;
+                    result.push(Node::PrintCurrPosNode);
                 }
                 Token::LeftBracketToken => {
                     let rest = self.input.clone();
-                    self.position += 1;
-                    let pos = self.position.clone();
+                    let pos = self.position.clone() + 1;
 
-                    // find next ] sign
-                    let idx = rest
+                    // find next ] sign with an offset
+                    let (_, right) = rest.split_at(pos);
+                    let idx = right
                         .clone()
                         .iter()
                         .position(|&r| r == Token::RightBracketToken)
                         .unwrap();
 
-                    let asdf = self.parse_expr(&rest[pos..idx], true);
+                    let rest = self.input.clone();
+                    let real_idx = idx + pos;
 
-                    x.push(asdf);
+                    let new_parsed = self.parse_expr(&rest[pos..real_idx], true);
+                    result.push(new_parsed);
 
                     // skip loop-length times
-                    iq.nth(idx - pos - 1);
+                    vect_iter.nth(real_idx - pos - 1);
                 }
                 Token::RightBracketToken => {
-                    x.push(Node::LoopCloseNode);
-                    self.position += 1;
+                    result.push(Node::LoopCloseNode);
                 }
             }
+            self.position += 1;
         }
 
         if is_loop == true {
-            Node::LoopExpr(Box::new(x))
+            Node::LoopExpr(Box::new(result))
         } else {
-            Node::Expr(Box::new(x))
+            Node::Expr(Box::new(result))
         }
     }
 
     pub fn parse_all(&mut self) -> Node {
-        let x = self.parse_expr(&self.input.clone(), false);
-        // println!("{:#?}", x);
+        self.parse_expr(&self.input.clone(), false)
+    }
+}
 
-        x
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use super::*;
+
+    static SIMPLE_NOLOOP_FILE: &str = "simple_noloop_test.bf";
+    static SIMPLE_LOOP_FILE: &str = "simple_loop_test.bf";
+
+    #[test]
+    fn test_valid_simple_noloops() {
+        let mut file = File::create(SIMPLE_NOLOOP_FILE).unwrap();
+        file.write_all(b"+.-").unwrap();
+
+        let lx = Lexer::new(SIMPLE_NOLOOP_FILE).unwrap();
+        let mut px = Parser::new(lx.tokens);
+
+        let res = Node::Expr(Box::new(vec![
+            Node::PlusNode,
+            Node::PrintCurrPosNode,
+            Node::MinusNode,
+        ]));
+
+        assert_eq!(px.parse_all(), res);
+        fs::remove_file(SIMPLE_NOLOOP_FILE).unwrap();
+    }
+
+    #[test]
+    fn test_valid_simple_loop() {
+        let mut file = File::create(SIMPLE_LOOP_FILE).unwrap();
+        file.write_all(b"+.[-]+").unwrap();
+
+        let lx = Lexer::new(SIMPLE_LOOP_FILE).unwrap();
+        let mut px = Parser::new(lx.tokens);
+
+        let res = Node::Expr(Box::new(vec![
+            Node::PlusNode,
+            Node::PrintCurrPosNode,
+            Node::LoopExpr(Box::new(vec![Node::MinusNode])),
+            Node::LoopCloseNode,
+            Node::PlusNode,
+        ]));
+
+        assert_eq!(px.parse_all(), res);
+        fs::remove_file(SIMPLE_LOOP_FILE).unwrap();
     }
 }
